fdc3b176eadb4f2bb39a676e9daa1750d81acc58
NicolasAyal <javier17utn@gmail.com>
2023-11-01 11:38:20 -0300

    nav desing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import "./App.scss";
+
 import Logo from "./Components/Logo";
 
+import { FiAirplay,FiCoffee,FiCodesandbox,FiDribbble,FiMail } from "react-icons/fi";
+
 function App() {
+
+  const [menu, setMenu] = useState('home')
+  
   return (
     <>
       <main className="main-black">
@@ -10,26 +17,42 @@ function App() {
         </section>
         <header>
           <nav>
-            <a href="#home">
-              <span className="icon"></span>
+            <a href="#home" 
+            className={`${menu == 'home'?'active':''}`}
+            onClick={()=>{setMenu('home')}}
+            >
+              <span className="icon"><FiAirplay/></span>
               <span className="text">Home</span>
             </a>
-            <a href="#about">
-              <span className="icon"></span>
+            <a href="#about" 
+            className={`${menu == 'about'?'active':''}`}
+            onClick={()=>{setMenu('about')}}
+            >
+              <span className="icon"><FiCoffee/></span>
               <span className="text">About</span>
             </a>
-            <a href="#project">
-              <span className="icon"></span>
+            <a href="#project" 
+            className={`${menu == 'project'?'active':''}`}
+            onClick={()=>{setMenu('project')}}
+            >
+              <span className="icon"><FiCodesandbox/></span>
               <span className="text">Project</span>
             </a>
-            <a href="#games">
-              <span className="icon"></span>
+            <a href="#games" 
+            className={`${menu == 'games'?'active':''}`}
+            onClick={()=>{setMenu('games')}}
+            >
+              <span className="icon"><FiDribbble/></span>
               <span className="text">Games</span>
             </a>
-            <a href="#contact">
-              <span className="icon"></span>
+            <a href="#contact" 
+            className={`${menu == 'contact'?'active':''}`}
+            onClick={()=>{setMenu('contact')}}
+            >
+              <span className="icon"><FiMail/></span>
               <span className="text">Contact</span>
             </a>
+            <div className="indicador"></div>
           </nav>
         </header>
         <section className="home" id="home">Home</section>
