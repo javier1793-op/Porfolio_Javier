57304eb9063704c6b4a611ef8abd9b517f504f59
NicolasAyal <javier17utn@gmail.com>
2023-11-07 10:59:59 -0300

    about-finish

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,26 +1,41 @@
 import "../Scss/about.scss";
+import DataPersonal from "./DataPersonal";
 
 const About = () => {
+ 
   return (
     <>
       <div className="contentAbout">
-        
-          <div className="titleAbout"> 
+        <div className="titleAbout">
           <h1>Who I am</h1>
-          </div>
-          <div className="descriptionAbout"> 
-          <p>I'm a passionate frontend web developer with a focus on creating engaging and functional user experiences. My journey in the world of programming began with curiosity and a desire to learn. Throughout my learning process, I've acquired skills in HTML, CSS, and JavaScript, and I've applied this knowledge to personal projects ranging from interactive websites to simple web applications.</p>
+        </div>
+        <div className="descriptionAbout">
+          <p>
+            I&apos;m a passionate frontend web developer with a focus on creating
+            engaging and functional user experiences. My journey in the world of
+            programming began with curiosity and a desire to learn. Throughout
+            my learning process, I&apos;ve acquired skills in HTML, CSS, and
+            JavaScript, and I&apos;ve applied this knowledge to personal projects
+            ranging from interactive websites to simple web applications.
+          </p>
           <p>
             <br />
-          What motivates me the most is the opportunity to translate ideas into accessible and efficient technological solutions. I'm committed to continuous learning and always seek opportunities to improve my technical skills and explore the latest trends in web development.
+            What motivates me the most is the opportunity to translate ideas
+            into accessible and efficient technological solutions. I&apos;m committed
+            to continuous learning and always seek opportunities to improve my
+            technical skills and explore the latest trends in web development.
           </p>
           <br />
-          <p>My goal is to continue growing as a frontend web developer and contribute to innovative projects in this field. I'm excited about the opportunities that web development offers and am ready to take on new challenges in this exciting journey.</p>
-          </div>
-          <div className="infoAbout"> 
-          <p>informacion personal</p>
-          </div>
-       
+          <p>
+            My goal is to continue growing as a frontend web developer and
+            contribute to innovative projects in this field. I&apos;m excited about
+            the opportunities that web development offers and am ready to take
+            on new challenges in this exciting journey.
+          </p>
+        </div>
+        <div className="infoAbout">
+          <DataPersonal />
+        </div>
       </div>
     </>
   );
---
+++ src/Components/DataPersonal.jsx
@@ -0,0 +1,51 @@
+import "../Scss/dataPersonal.scss";
+import { FiDownload } from "react-icons/fi";
+
+const DataPersonal = () => {
+  const Data = [
+    {
+      id: 1,
+      label: "Name:",
+      data: "Ruíz Díaz Javier A.",
+    },
+    {
+      id: 2,
+      label: "Email:",
+      data: "javier17utn@gmail.com",
+    },
+    {
+      id: 3,
+      label: "Country:",
+      data: "Argentina",
+    },
+    {
+      id: 4,
+      label: "Phone:",
+      data: "+54 3704264610",
+    },
+    {
+      id: 5,
+      label: "Qualification:",
+      data: "system analyst",
+    },
+    {
+      id: 6,
+      label: "Studying:",
+      data: "Systems Engineering - UTN",
+    },
+  ];
+
+  return (
+    <>
+      {Data?.map((item) => (
+        <div className="dataPersonal" key={item.id}>
+          <span>{item.label}</span>
+          <span>{item.data}</span>
+        </div>
+      ))}
+      <button className="btn-cv">downloand CV <FiDownload className="iconDownload"/></button>
+    </>
+  );
+};
+
+export default DataPersonal;
