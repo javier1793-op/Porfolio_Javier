ccb189439110a20f95ac7cbf30b848a8fd7610e7
NicolasAyal <javier17utn@gmail.com>
2023-11-11 13:09:24 -0300

    list-project

--- src/Components/Cardproject.jsx
+++ src/Components/Cardproject.jsx
@@ -1,23 +1,22 @@
 import '../Scss/cardProject.scss'
-import Anime from '../Img/project/Anime.png'
+import PropTypes from 'prop-types';
 
-const Cardproject = () => {
-    const imgUrl= Anime;
-
+const Cardproject = ({imgUrl,title,description}) => {
+  
+    
     const estiloComponente = {
         backgroundImage: `url(${imgUrl})`,
         backgroundSize: 'cover', 
-       
+        backgroundRepeat: 'no-repeat',
       };
 
   return (
     <>
       <div className="card" style={estiloComponente}>
         <div className="card__content">
-          <h2 className="card__title">Anime Page</h2>
+          <h2 className="card__title">{title}</h2>
           <p className="card__text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
-            eligendi perferendis similique!
+           {description}
           </p>
           <a href="#" className="btn">
             See More
@@ -27,5 +26,9 @@ const Cardproject = () => {
     </>
   );
 };
-
+Cardproject.propTypes = {
+    imgUrl: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  };
 export default Cardproject;
--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,19 +1,53 @@
 import '../Scss/project.scss'
 import Cardproject from './Cardproject';
 
+import Anime from '../Img/project/Anime.png'
+import Serendipia from '../Img/project/serendipia.png'
+import Things from '../Img/project/things.png'
+import Weather from '../Img/project/weather.png'
+
 const Project = () => {
+
+    const ListProject = [
+        {
+            id:100,
+            urlImg:`${Anime}`,
+            title:'Page Anime',
+            description:'Pagina de busqueda de anime para busqueda y seguimiento de los mismo',
+        },
+        {
+            id:101,
+            urlImg:`${Serendipia}`,
+            title:'Page Diary Personal',
+            description:'Pagina de diario personal videojuego para busqueda y seguimiento de los mismo',
+        },
+        {
+            id:102,
+            urlImg:`${Things}`,
+            title:'Page Things',
+            description:'Pagina de Notas Recordatorio para busqueda y seguimiento de los mismo',
+        },
+        {
+            id:103,
+            urlImg:`${Weather}`,
+            title:'Page Weather',
+            description:'Pagina del Clima para busqueda y seguimiento de los mismo',
+        },
+    ]
     return (
         <>
           <div className="titleAbout">
             <h1>My projects</h1>
           </div>
           <div className="contentProject">
-            <Cardproject/>
-            <Cardproject/>
-            <Cardproject/>
-            <Cardproject/>
-            <Cardproject/>
-            <Cardproject/>
+            {ListProject.map((item)=>(
+                <Cardproject 
+                key={item.id} 
+                imgUrl={item.urlImg}
+                title={item.title}
+                description={item.description}
+                />
+            ))}
           </div>
         </>
       );
