ccf3b54a09268b308d5264b8ed1c18c9bbee78b1
NicolasAyal <javier17utn@gmail.com>
2023-11-21 12:23:00 -0300

    contact-gmail-emailjs

--- src/Components/Formcontact.jsx
+++ src/Components/Formcontact.jsx
@@ -1,16 +1,33 @@
 import "../Scss/formContact.scss";
+import  { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
 import { LuSend } from "react-icons/lu";
 import { FaWhatsapp,FaMailBulk } from "react-icons/fa";
 
 const Formcontact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_ht2egk6', 'Contact_Form', form.current, 'd1coGa8C7PSe-s7fy')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
   return (
     <div className="contentForm">
       <h1 className="titleContact">Contact Me</h1>
-      <form className="form">
-        <input type="text" placeholder="name" />
-        <input type="text" placeholder="mail" />
+      <form className="form" ref={form} onSubmit={sendEmail}>
+        <input type="text" placeholder="name" name="user_name"/>
+        <input type="text" placeholder="mail" name="user_email"/>
         <textarea
-          name="descriptionForm"
+          name="message"
           id=""
           cols="30"
           rows="3"
